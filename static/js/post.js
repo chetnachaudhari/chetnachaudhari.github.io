9c79784db426014cc7789442d89724af59730b27
chetnachaudhari <chetnachaudhari@gmail.com>
2016-03-03 17:47:08 +0530

    First layout

---
+++ static/js/post.js
@@ -0,0 +1,38 @@
+---
+    layout: null
+---
+
+$(document).ready(function() {
+    generateContent();
+    shareButtons();
+    disqus();
+});
+
+function shareButtons() {
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+}
+
+function generateContent() {
+    var $mt = $('.toc');
+    var $toc;
+    $mt.each(function(i,o){
+        $toc = $(o);
+        $toc.toc({ listType: 'ul', headers: 'h1, h2, h3' });
+    });
+}
+
+function disqus(){
+    var disqus_shortname = '{{site.disqus_shortname}}';
+
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    document.getElementsByTagName("script")[0].parentNode.appendChild(dsq);
+}
